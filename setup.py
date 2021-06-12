221ab909e120b308a3e51194f835a9259793c7d9
sarvesh4396 <georgiansarvesh4396@gmail.com>
2021-06-12 14:48:59 +0530

    Completed setup.py

---
+++ setup.py
@@ -0,0 +1,56 @@
+from __future__ import print_function
+from os import name
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_long_description():
+    with open("README.md") as f:
+        long_description = f.read()
+    return long_description
+
+
+# Package meta-data.
+NAME = "pantry-cloud"
+DESCRIPTION = "A Modular Penetration Testing Framework"
+KEYWORDS = [NAME, "JSON", "API", "CLOUD STORAGE"]
+GIT_URL = "https://github.com/sarvesh4396/pantry-cloud"
+AUTHOR = "Sarvesh Kumar Dwivedi"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "3.13.0"
+
+
+setup(
+    name=NAME,
+    packages=[NAME],
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    python_requires=REQUIRES_PYTHON,
+    url=GIT_URL,
+    download_url='https://github.com/sarvesh4396/pantry-cloud/archive/refs/tags/V_01.tar.gz',
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    entry_points={},
+    install_requires=["requests"],
+    include_package_data=True,
+    license="MIT",
+    keywords=KEYWORDS,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Communications :: File Sharing",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
+
